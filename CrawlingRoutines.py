75b259c7061dd3fdc5aaa89013b40e1018e175b3
Steve <slarge@sfu.ca>
2017-10-21 14:37:41 -0700

    First Commit

---
+++ CrawlingRoutines.py
@@ -0,0 +1,26 @@
+#This python script contains ancillary web crawler subroutines
+#
+#Steven Large
+#October 21st 2017
+
+from urllib.request import urlopen
+
+from bs4 import BeautfulSoup
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def GetInternalLinks(url):
+
+	return internalLinks
+
+
+def GetExternalLinks(url):
+
+	return externalLinks
+
+
+def GenerateSiteMap(TopURL):
+
+	return Pages, Connections
+
